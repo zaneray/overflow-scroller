6a9a5d7fbf45bfb756d0ccc6285699bb88cecf13
Jeremiah Martin <jeremiah@zaneray.com>
2016-10-31 13:30:41 -0600

    Adding example images, as well as supporting multiple instances of scroller on a page, support for a carousel look as well.

--- overflow-scroller.js
+++ overflow-scroller.js
@@ -1,60 +1,49 @@
 /*
 $.overflowScroller();
 Dependencies: 
-jquery, 
+jquery;
 jquery-throttle-debounce
 Modernizr.hiddenscroll property
 
-
 adds scrolling arrows to elements with overflow scroll, 
 all elements must be the same width
-
 */
 
 (function ($) {
   $.fn.overflowScroller = function (args) {
-    var options = {
-      scrollPercentage: 0.9,
-      scrollSpeed: 400
 
+    var options = {
+      scrollPercentage: 1,
+      scrollSpeed: 400,
+      snapPoints: true
     };
 
-
-    var scrollContainerWidth, 
-    scrollElementWidth, 
-    scrollElementWidth, 
-    scrollElementTotalWidth,
-    scrollPosition,
-    isClicked = false,
-    arrowHiddenClass = 'overflow-scroller-arrow-hide';
-
-
+    var arrowHiddenClass = 'overflow-scroller-arrow-hide';
+        
     $.extend( options, args );
 
-    $this = this;
-
-
-
-
     this.each(function(){
-      var $overflowScroller = $(this);
+      var $overflowScroller = $(this),
+          scrollContainerWidth = $overflowScroller.width(), 
+          scrollElementWidth = $overflowScroller.children(':eq(0)').width(),
+          scrollElementTotalWidth = scrollElementWidth * $overflowScroller.children().length,
+          scrollPosition = 0,
+          isClicked = false;
+
+      $overflowScroller.toggleClass('snap-points', options.snapPoints);
+
       $overflowScroller.wrap('<div class="overflow-scroller-outer"></div>');
       
-      var $overflowScrollerOuter = $this.parent('.overflow-scroller-outer');
-
-
+      var $overflowScrollerOuter = $overflowScroller.parent('.overflow-scroller-outer');
 
       $overflowScrollerOuter.append('<div class="overflow-scroller-arrow overflow-scroller-arrow-previous overflow-scroller-arrow-hide"></div><div class="overflow-scroller-arrow overflow-scroller-arrow-next overflow-scroller-arrow-hide"></div>');
 
-      var $arrows         = $overflowScrollerOuter.find('.overflow-scroller-arrow'),
+      var $arrows     = $overflowScrollerOuter.find('.overflow-scroller-arrow'),
       $arrowPrevious  = $overflowScrollerOuter.find('.overflow-scroller-arrow-previous'),
       $arrowNext      = $overflowScrollerOuter.find('.overflow-scroller-arrow-next');
 
-
       var setScrollArrows = function(){
         scrollPosition = $overflowScroller.scrollLeft();
-        //console.log('scrollPosition: ' + scrollPosition);
-        //console.log('scrollElementTotalWidth - scrollContainerWidth: ' + (scrollElementTotalWidth - scrollContainerWidth));
         if(scrollPosition >= (scrollElementTotalWidth - scrollContainerWidth)) {
           $arrowNext.addClass(arrowHiddenClass);
         }
@@ -66,33 +55,9 @@ all elements must be the same width
         }
       };
 
-
-      // if(!Modernizr.hiddenscroll) {
-      //   $overflowScroller.addClass('hidden-scroll');
-
-        //$(window).load(function(){ 
-          //TODO this isn't quite right yet but it should already work well with the class
-        // console.log('$overflowScrollerOuter: ' + $overflowScrollerOuter.height());
-        // console.log('$overflowScroller: ' + $overflowScroller.children(':eq(0)').height());
-        // var heightDifference = (($overflowScrollerOuter.height() - $overflowScroller.children(':eq(0)').height()) + -17);
-        // console.log('heightDifference: ' + heightDifference); 
-
-        // $overflowScroller.css('margin-bottom', heightDifference + 'px');
-        
-      // }
-
-      scrollContainerWidth = $overflowScroller.width();
-      scrollElementWidth = $overflowScroller.children(':eq(0)').width();
-      scrollElementTotalWidth = scrollElementWidth * $this.children().length;
-     //console.log('scrollContainerWidth: ' + scrollContainerWidth);
-     // console.log('scrollElementWidth: ' + scrollElementWidth);
-     //console.log('scrollElementTotalWidth: ' + scrollElementTotalWidth);
-
-
      if(scrollElementTotalWidth > scrollContainerWidth) {
-      //if the total width is great than the container show the arrows and trigger functionality
+      //if the total width is greater than the container show the arrows and trigger functionality
       scrollPosition = $overflowScroller.scrollLeft();
-      //console.log('scrollPostion: ' + scrollPosition);
 
       //detect initial scroll postion and show/hide arrows. 
       if(scrollPosition === 0) {
@@ -102,7 +67,7 @@ all elements must be the same width
         $arrows.removeClass(arrowHiddenClass);
       }
 
-
+      //bind events
       $arrowNext.on('click',function(){
         if(!isClicked) {
           $arrowPrevious.removeClass(arrowHiddenClass);
@@ -133,19 +98,23 @@ all elements must be the same width
         }
       });
 
-
-      if($.debounce != undefined) {
+      if($.debounce !== undefined) {
         $overflowScroller.scroll($.debounce( 100, function(){
           setScrollArrows();
         }));
       } else {
         //debounce is not loaded. 
-        $overflowScroller.scroll( 100, function(){
-          setScrollArrows();
-        });
+        $overflowScroller.scroll(setScrollArrows);
       }
+
+      //reset total values on resize
+      $(window).on('resize.overflowScroller',function(){
+        scrollContainerWidth      = $overflowScroller.width();
+        scrollElementWidth        = $overflowScroller.children(':eq(0)').width();
+        scrollElementTotalWidth   = scrollElementWidth * $overflowScroller.children().length;
+      });
     } 
-  }); //end each
+  }); 
   };
 })( jQuery );
 
